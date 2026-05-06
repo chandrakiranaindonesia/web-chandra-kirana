10448e8147c1535df8e8f7701e7db235f6887904
Surya <saelelite@gmail.com>
2026-05-06 16:21:47 +0700

    Menambahkan vite.config.js untuk multi-page routing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tentang: resolve(__dirname, 'tentang-kami.html'),
+        layanan: resolve(__dirname, 'layanan.html'),
+        portofolio: resolve(__dirname, 'portofolio.html'),
+        legalitas: resolve(__dirname, 'legalitas.html'),
+        testimoni: resolve(__dirname, 'testimoni.html'),
+        kontak: resolve(__dirname, 'kontak.html')
+      }
+    }
+  }
+});
